5da25072fabb8d253488c80191f8f3835b2839a5
lihui <lih7253@gmail.com>
2023-07-08 22:40:52 +0800

    创建菜单“

---
+++ web/src/components/ContentField.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container content-field">
+        <div class="card">
+            <card class="card-body">
+                <slot></slot>
+            </card>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+div.content-field{
+    margin-top: 20px;
+}
+</style>
