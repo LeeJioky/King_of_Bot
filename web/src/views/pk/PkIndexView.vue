f85c848b40e24bdf11dfca19a8d42fbe801dbd9b
lihui <lih7253@gmail.com>
2023-07-18 19:12:05 +0800

    实现了导航栏+对战页面的地图和障碍物

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,14 +1,12 @@
 <template>
-    <ContentField>
-        对战
-    </ContentField>
+    <PlayGround />
 </template>
 <script>
-import ContentField from '../../components/ContentField'
+import PlayGround from '@/components/PlayGround.vue';
 export default {
     components: {
-        ContentField
-    }
+    PlayGround
+}
 }
 </script>
 <style scoped>
