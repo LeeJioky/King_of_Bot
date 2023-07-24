811418443c00a52dc66d153a49ad2b64eaa1cf36
lihui <lih7253@gmail.com>
2023-07-24 13:39:48 +0800

    后端api实现完毕:rocket:“

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,9 +7,55 @@
 import NavBar from '@/components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+
+import $ from 'jquery'
 export default{
   components:{
     NavBar
+  },
+  setup(){
+    $.ajax({
+      url: "http://localhost:3000/user/account/token/",
+      type: "post",
+      data: {
+        username: "lihui",
+        password: "plihui"
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    }),
+    $.ajax({
+      url: "http://localhost:3000/user/account/info/",
+      type: "get",
+      headers: {
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwZDIxZTRkZTBkZmY0ZTBhOTMzMGViYjAzMmRlMDIwYyIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY5MDE3MzM4NiwiZXhwIjoxNjkxMzgyOTg2fQ.I9GGAMErMjpVd1h-cwT13y8FIHtYEkax1bffLHrtpGw"
+      },
+      success(resp){
+        console.log(resp)
+      },
+      error(resp){
+        console.log(resp)
+      }
+    }),
+    $.ajax({
+      url: "http://localhost:3000/user/account/register/",
+      type: "post",
+      data: {
+        username: "zhanghua",
+        password: "pzh",
+        confirmedPassword: "pzh"
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    })
   }
 }
 </script>
